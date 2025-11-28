1b0cd5c4415c3ee51e4d53d96b16a5cd9d44d79d
keshav kumar <keshavks9810@gmail.com>
2025-11-29 00:38:06 +0530

    Initializing Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">Hi keshav</h1>
+    </div>
+  )
+}
+
+export default App
